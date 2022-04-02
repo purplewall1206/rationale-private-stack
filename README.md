624756de5d236dcec2694d39969fc9a1e801dbe6
purplewall1206 <wzc@smail.nju.edu.cn>
2022-04-02 17:12:30 +0800

    private stack done

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 
 A simple example of a x64 private stack for sensitive functions
 
+只考虑参数6个以内，返回值8字节以内的情况，即只需要移动RSP寄存器即可
+
+> 方案二，整体加减偏移量，平移RBP，RSP寄存器
 
 ## parameters passing
 
@@ -11,7 +14,7 @@ x64的cpu上有足够的寄存器，因此
 > System V ABI:  
 >> The first six integer or pointer arguments are passed in registers RDI, RSI, RDX, RCX, R8, and R9, while XMM0, XMM1, XMM2, XMM3, XMM4, XMM5, XMM6 and XMM7 are used for floating point arguments ... As in the Microsoft x64 calling convention, additional arguments are passed on the stack and the return value is stored in RAX.
 
-据观察在linux，更多的参数都是寄存器传递的，但是规则
+超过六个寄存器，或者struct类型返回值超过16字节，使用栈传递
 
 
 ## function disassembler
@@ -22,4 +25,4 @@ the start of a function
     mov  %rsp, %rbp
 ```
 
-因此可以试试看只换一个$rsp 有没有影响
+
--- src/main.c
+++ src/main.c
@@ -4,15 +4,60 @@
 unsigned long RSP0 = 0;
 unsigned long RSP1 = 0;
 
+// sensitive enter in to the non-sensitive
+#define s_enter_ns \
+        asm volatile ("mov %%rsp, %0" : "=m"(RSP1):); \
+        asm volatile ("mov %0, %%rsp": :""(RSP0)); 
+
+#define ns_exit_s \
+        asm volatile ("mov %0, %%rsp": :""(RSP1));
+
+#define ns_enter_s \
+        asm volatile ("mov %%rsp, %0" : "=m"(RSP0):);\
+        asm volatile ("mov %0, %%rsp" : :""(RSP1));
+
+#define s_exit_ns \
+        asm volatile ("mov %0, %%rsp" : :""(RSP0));
+
+void nested_test(int a, int b, int c, int d);
+
 unsigned long service(unsigned long c) {
+    printf("request service\n");
     return c + 2;
 }
 
 unsigned long sensitive(unsigned long a, unsigned long b) {
     unsigned long c = a + b;
-    printf("sensitive %d\n", c);
-    // c = service(c);
+    printf("sensitive %ld\n", c);
+
+    s_enter_ns
+    c = service(c);
+    ns_exit_s
+
+    s_enter_ns
+    c = service(c);
+    ns_exit_s
+
+	s_enter_ns
+    nested_test(10, 100, 1000, 4096);
+    ns_exit_s
     return c;
+}
+
+void sensitive_nested_test(int a, int b, int c, int d)
+{
+	printf("sensitive_nested_test %d %d %d %d\n", a, b, c, d);
+	s_enter_ns
+    service(10);
+    ns_exit_s
+}
+
+void nested_test(int a, int b, int c, int d)
+{
+	printf("nested_test %d %d %d %d\n", a, b, c, d);
+	ns_enter_s
+    sensitive_nested_test(a, b, c, d);
+    s_exit_ns
 }
 
 
@@ -21,15 +66,19 @@ int main()
     unsigned long x = 2;
     unsigned long y = 8;
 
+    // create a private stack, put the rsp in the middle
+    // if put rsp in the beginning, 'push' will cause a illegal access, and result in segment fault
     unsigned long *addr = (unsigned long*) malloc(4096 * 4);
-    RSP1 = (unsigned long)addr;
+    RSP1 = (unsigned long)(addr + 4096 * 2);
 
-    asm("mov %%rsp, %0" : "=r"(RSP0):);
-    asm("mov %0, %%rsp" : :"r"(RSP1));
+    // asm volatile ("mov %%rsp, %0" : "=m"(RSP0):);
+    // asm volatile ("mov %0, %%rsp" : :""(RSP1));
+    ns_enter_s
     unsigned long z = sensitive(x, y);
-    asm("mov %0, %%rsp" : "=r"(RSP0):);
+    s_exit_ns
+    // asm volatile ("mov %0, %%rsp" : :""(RSP0));
 
-    printf("main %d\n", z);
+    printf("main %ld\n", z);
 
-    return (int)z;
+    return 0;
 }
